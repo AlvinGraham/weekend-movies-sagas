fb86da31bfbbf4748f6413e5dcaecd4d3ce5a77d
Alvin Graham <grah0071@gmail.com>
2024-03-05 07:06:42 -0600

    Add and test server genre/:id GET

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -18,4 +18,24 @@ router.get('/', (req, res) => {
     });
 });
 
+// get genres for specific movie
+router.get('/:id', (req, res) => {
+  // set query text and args
+  const queryText = `SELECT "genres".id, "genres".name FROM "genres"
+  JOIN "movies_genres" ON "genres".id = "movies_genres".genre_id
+  WHERE "movies_genres".movie_id = $1;
+  `;
+  const queryArgs = [req.params.id];
+
+  pool
+    .query(queryText, queryArgs)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.error('ERROR in server genre/:id GET:', err);
+      res.sendStatus(500);
+    });
+});
+
 module.exports = router;
